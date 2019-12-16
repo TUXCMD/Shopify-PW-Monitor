bbc4fe948d67a05a3c6bb4f9e764ebb0b4865c74
Yuanxu Zhang <55955228+zyx898@users.noreply.github.com>
2019-12-16 00:33:40 -0600

    Update pw_monitor.py

--- pw_monitor.py
+++ pw_monitor.py
@@ -1,16 +1,19 @@
 import time
+import random
 import requests
-from bs4 import BeautifulSoup
-from discord_webhook import DiscordEmbed, DiscordWebhook
+import threading
 from datetime import datetime
+from bs4 import BeautifulSoup
+from dhooks import *
 
 
-monitor_store_url = 'https://eflash-us.doverstreetmarket.com'
+urls = ['https://eflash-us.doverstreetmarket.com','https://shop.travisscott.com','https://bdgastore.com','https://shop-usa.palaceskateboards.com','https://dropsau.com/','https://eflash-jp.doverstreetmarket.com/','https://wearebraindead.com','https://eflash-sg.doverstreetmarket.com','https://shop-jp.palaceskateboards.com/']
 
+webhook_url = ''
 
-webhook_url = ''#Input Your Webhook URL
+error_url = ''#Input Your Webhook URL
 
-delay = '2' # number 1-10
+delay = '5' # number 1-10
 
 headers = {
 
@@ -19,57 +22,61 @@ headers = {
 
 
 def message_post(pw_status,monitor_store_url):
-
-		webhook = DiscordWebhook(url=webhook_url,content='')
-		
-		embed = DiscordEmbed(title=pw_status, color=0x00fea9,url=monitor_store_url)
-		
-		embed.add_embed_field(name=monitor_store_url,value='**'+str(pw_status)+'**')
-				
-		embed.set_footer(text='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
-		
-		embed.set_timestamp()
-		
-		webhook.add_embed(embed)
-		
-		webhook.execute()
-		
-		print('[SUCCESS] --> Successfully sent success webhook!')
+    hook = Webhook(webhook_url)
+    embed = Embed(description='**'+str(pw_status)+'**',color=0x36393F,timestamp='now')
+    embed.set_title(title=str(monitor_store_url),url=monitor_store_url)
+    embed.set_footer(text='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
+    hook.send(embed=embed)
+    print('[SUCCESS] --> Successfully sent success webhook!')
 
 
-def main():
+def error_post(message,monitor_store_url):
+    hook = Webhook(error_url)
+    embed = Embed(description='**'+str(message)+'**',color=0x36393F,timestamp='now')
+    embed.set_title(title=str(monitor_store_url),url=monitor_store_url)
+    embed.set_footer(text='@zyx898',icon_url='https://pbs.twimg.com/profile_images/1118878674642714624/lNXTIWNT_400x400.jpg')
+    hook.send(embed=embed)
+    print('[SUCCESS] --> Successfully sent success webhook!')
 
-	last_status = ''
 
-	while True:
+def main(url):
+    last_status = ''
 
-		page_source = requests.get(monitor_store_url,headers=headers)
+    while True:
+        try:
+            page_source = requests.get(url,headers=headers)
 
-		page_text = BeautifulSoup(page_source.text,"html.parser")
+            page_text = BeautifulSoup(page_source.text,"lxml")
 
-		if 'Opening Soon' in page_text.title.text:
+            if 'Opening Soon' in page_text.title.text:
 
-			pw_status = "Password Page Up"
+                pw_status = "Password Page Up :lock:"
 
-			if pw_status != last_status:
+                if pw_status != last_status:
 
-				message_post(pw_status,monitor_store_url)
+                    message_post(pw_status,url)
 
-				last_status = pw_status
+                    last_status = pw_status
 
-		else:
+            else:
 
-			pw_status = "Password Page Down"
+                pw_status = "Password Page Down :unlock:"
 
-			if pw_status != last_status:
+                if pw_status != last_status:
 
-				message_post(pw_status,monitor_store_url)
+                    message_post(pw_status,url)
 
-				last_status = pw_status
+                    last_status = pw_status
 
-		now = datetime.now()
+            now = datetime.now()
 
-		print(str(now) + 'Monitoring-------------------[ '+last_status+' ]')
+            print(str(now) + ' Monitoring-------------------[ '+str(url)+'   '+last_status+' ]')
 
-		time.sleep(int(delay))
-main()
+            time.sleep(int(delay))
+        except:
+            print('Error Requesting to This site   ',url)
+            time.sleep(5)
+
+if __name__ == "__main__":
+    for i in urls:
+        threading.Thread(name=str(i),target=main,args=(i,)).start()
